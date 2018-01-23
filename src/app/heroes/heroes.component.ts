8c6972acb9173ef37de7ad14a175f1c4eafb6b58
Mona Lisa <Mona.Lisa@geemail.com>
2018-01-23 18:07:41 +0530

    add a new hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -22,4 +22,13 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroSerive.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
 }
